b63fdafd2af499895bf6982b39ba24edf061b6e7
lucasrafaeldasilvatorres-create <lucasrafaeldasilvatorres@gmail.com>
2026-04-26 08:09:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function girar() {
+  let nomes = document.getElementById("nomes").value.split(",");
+  let numTimes = parseInt(document.getElementById("times").value);
+
+  if (nomes.length < 2 || !numTimes) {
+    alert("Preencha corretamente!");
+    return;
+  }
+
+  nomes = nomes.map(n => n.trim());
+
+  let roleta = document.getElementById("roleta");
+
+  // animação girando
+  let graus = 360 * 5 + Math.floor(Math.random() * 360);
+  roleta.style.transform = `rotate(${graus}deg)`;
+
+  // som
+  let som = document.getElementById("som");
+  som.play();
+
+  setTimeout(() => {
+    // embaralhar
+    nomes.sort(() => Math.random() - 0.5);
+
+    let resultado = "";
+
+    let tamanho = Math.ceil(nomes.length / numTimes);
+
+    for (let i = 0; i < numTimes; i++) {
+      let time = nomes.slice(i * tamanho, (i + 1) * tamanho);
+
+      resultado += `\nEquipe ${i + 1}:\n`;
+      time.forEach(nome => {
+        resultado += `- ${nome}\n`;
+      });
+    }
+
+    document.getElementById("resultado").innerText = resultado;
+  }, 3000);
+}
